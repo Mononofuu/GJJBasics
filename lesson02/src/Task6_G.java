dde610de97563b2b6003cfb1a37bfb7323775ac1
Mononofuu <mononofuu@gmail.com>
2014-12-21 00:12:44 +0200

    [DONE] Lesson02 Task6_G

---
+++ lesson02/src/Task6_G.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Дано целое число A и натуральное число n.
+ * Выведите число, которое состоит только из n последних бит числа A
+ * (то есть обнулите все биты числа A, кроме последних n).
+ */
+public class Task6_G {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = sc.nextInt();
+        System.out.println(Integer.toBinaryString((a >> i) << i));
+    }
+}
