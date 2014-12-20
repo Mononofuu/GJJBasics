55892c2772971b9e30e90549d34f7cfd9876081c
Mononofuu <mononofuu@gmail.com>
2014-12-20 23:23:25 +0200

    [DONE] Lesson02 Task6_D

---
+++ lesson02/src/Task6_D.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Дано целое число A и натуральное число i.
+ * Выведите число, которое получается из числа A установкой значения i-го бита равному 1.
+ */
+public class Task6_D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = sc.nextInt();
+        System.out.println(Integer.toBinaryString(a | powerOfTwo(i - 1)));
+    }
+
+    static int powerOfTwo(int number) {
+        return number == 0 ? 1 : 2 << number - 1;
+    }
+}
