e21cf8b0eab7af9d16487ee1cb1cc22804fed004
Mononofuu <mononofuu@gmail.com>
2014-12-20 21:55:04 +0200

    [DONE] Lesson02 Task6_A

---
+++ lesson02/src/Task6_A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Дано число n<32. Запишите число 2^n, то есть число, у которого n-й бит равен 1, а остальные – нули.
+ */
+public class Task6_A {
+    public static void main(String[] args) {
+        System.out.println("Введите n");
+        int n = new Scanner(System.in).nextInt();
+        int result = n == 0 ? 1 : 2 << n - 1;
+        System.out.println("2^" + n + "=" + result);
+    }
+}
