39e3bc0a24ef48cc378dd0b3980d850a211fc5f3
Mononofuu <mononofuu@gmail.com>
2014-12-21 00:10:30 +0200

    [DONE] Lesson02 Task6_C

---
+++ lesson02/src/Task6_C.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Дано целое число A и натуральное число i.
+ * Обнулите у числа A его последние i бит и выведите результат.
+ */
+public class Task6_C {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = sc.nextInt();
+        System.out.println(Integer.toBinaryString((a << i) >>> i));
+    }
+}
