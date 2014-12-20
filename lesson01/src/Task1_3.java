0378627cd5acb26cb11bd60a8540a89e9c5bef43
Mononofuu <mononofuu@gmail.com>
2014-12-20 15:48:21 +0200

    Fixed and simplified

--- lesson01/src/Task1_3.java
+++ lesson01/src/Task1_3.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 
 public class Task1_3 {
     public static void main(String[] args) {
-        int number;
-        Scanner sc = new Scanner(System.in);
         try {
-            number = sc.nextInt();
-            System.out.println("Вы ввели число " + number);
-        }catch (InputMismatchException e) {
+            System.out.println("Вы ввели число " + new Scanner(System.in).nextInt());
+        } catch (InputMismatchException e) {
             System.out.println("Неверный ввод");
         }
     }
