9fa99c3575d9081723d6d290ab2d097cb16d1c51
Mononofuu <mononofuu@gmail.com>
2015-01-04 14:10:54 +0200

    [DELETE] Lesson05, Lesson07

--- lesson05/src/Task1.java
+++
@@ -1,9 +0,0 @@
-public class Task1 {
-    public static void main(String[] args) {
-        Assert.assertEquals("Привет, Иван!", getResult("Иван"));
-    }
-
-    private static String getResult(String name) {
-        return ("Привет, " + name + "!");
-    }
-}
--- lesson05/src/Task9_24.java
+++
@@ -1,9 +0,0 @@
-public class Task9_24 {
-    public static void main(String[] args) {
-        Assert.assertEquals("блок око", getResult("яблоко"));
-    }
-
-    private static String getResult(String word) {
-        return word.substring(1, 5) + " " + word.substring(3);
-    }
-}
